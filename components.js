4dfeb023c587258ed645e0facd13f0ec68524d32
learn123-han <haniahfatima@gmail.com>
2025-06-13 12:52:21 +0530

    Add files via upload

---
+++ components.js
@@ -0,0 +1,18 @@
+//import logo from './logo.svg';
+import './App.css';
+import FunctionalComp from "./Component/functionalcomponent";
+import {ClassComp,ClassComp1} from './Component/classComponent';
+function App() {
+  return (
+    <div>
+   <h1>Welcome to react</h1>
+   <h2>This is Haniah Fatima</h2>
+   <p> i am a fourth year compurt Science Engineering Student</p>
+   <FunctionalComp></FunctionalComp>
+   <ClassComp></ClassComp>
+   <ClassComp1></ClassComp1>
+   </div>
+  );
+}
+
+export default App;
